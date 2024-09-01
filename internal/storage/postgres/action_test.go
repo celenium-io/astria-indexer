5bc137e58809fd4a06f82c2f47d42381e1244234
Artem <aopoltorzhicky@gmail.com>
2024-09-01 13:22:07 +0300

    Fix: rollup_actions flag

--- internal/storage/postgres/action_test.go
+++ internal/storage/postgres/action_test.go
@@ -117,3 +117,27 @@ func (s *StorageTestSuite) TestActionByRollupAndBridge() {
 	s.Require().NotNil(action.Action.Data)
 	s.Require().NotNil(action.Action.Fee)
 }
+
+func (s *StorageTestSuite) TestActionByRollupAndBridgeWithoutRollupActions() {
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer ctxCancel()
+
+	actions, err := s.storage.Action.ByRollupAndBridge(ctx, 1, storage.RollupAndBridgeActionsFilter{
+		Sort:          sdk.SortOrderAsc,
+		Limit:         10,
+		Offset:        0,
+		RollupActions: false,
+		BridgeActions: true,
+	})
+	s.Require().NoError(err)
+	s.Require().Len(actions, 2)
+
+	action := actions[0]
+	s.Require().EqualValues(7316, action.Height)
+	s.Require().NotNil(action.Tx)
+	s.Require().NotNil(action.Action)
+	s.Require().EqualValues(1, action.Action.TxId)
+	s.Require().EqualValues(types.ActionTypeSequence, action.Action.Type)
+	s.Require().NotNil(action.Action.Data)
+	s.Require().NotNil(action.Action.Fee)
+}
