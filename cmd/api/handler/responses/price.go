9d6f21f04fbebef510bbbe06babca630ceaca4e3
Artem Poltorzhitskiy <aopoltorzhicky@gmail.com>
2025-04-23 16:15:14 +0200

    Fix: price entity (#103)

--- cmd/api/handler/responses/price.go
+++ cmd/api/handler/responses/price.go
@@ -11,16 +11,14 @@ import (
 )
 
 type Price struct {
-	CurrencyPair string    `example:"BTC/USDT"                  format:"string"    json:"currency_pair,omitempty" swaggertype:"string"`
-	Price        string    `example:"50.00"                     format:"string"    json:"price"                   swaggertype:"string"`
-	Time         time.Time `example:"2023-07-04T03:10:57+00:00" format:"date-time" json:"time"                    swaggertype:"string"`
+	Price string    `example:"50.00"                     format:"string"    json:"value" swaggertype:"string"`
+	Time  time.Time `example:"2023-07-04T03:10:57+00:00" format:"date-time" json:"time"  swaggertype:"string"`
 }
 
 func NewPrice(price storage.Price) Price {
 	return Price{
-		CurrencyPair: price.CurrencyPair,
-		Price:        price.Price.String(),
-		Time:         price.Time,
+		Price: price.Price.String(),
+		Time:  price.Time,
 	}
 }
 
