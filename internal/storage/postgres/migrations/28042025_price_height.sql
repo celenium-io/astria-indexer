08a39383703e93630734b744b4c642833ad76a24
Artem <aopoltorzhicky@gmail.com>
2025-04-28 17:17:11 +0300

    Fix: migration

--- internal/storage/postgres/migrations/28042025_price_height.sql
+++ internal/storage/postgres/migrations/28042025_price_height.sql
@@ -1,6 +1,7 @@
 update price 
 set height = blocks.height
 from (
-	select block.height, price.time from price
-	left join block on block.time = price.time
-) as blocks;
+	select height, time from block
+	where block.time >= '2025-04-01T00:00:00Z'
+) as blocks
+where blocks.time = price.time;
